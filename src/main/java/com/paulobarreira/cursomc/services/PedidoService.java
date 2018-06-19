a20f8239689dba3fb63e08c396f43657437bbfe8
Paulo Barreira <phfbarreira@gmail.com>
2018-06-18 22:06:20 -0300

    Endpoint /pedidos/{id} disponibilizado

---
+++ src/main/java/com/paulobarreira/cursomc/services/PedidoService.java
@@ -0,0 +1,23 @@
+package com.paulobarreira.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.paulobarreira.cursomc.domain.Pedido;
+import com.paulobarreira.cursomc.repositories.PedidoRepository;
+import com.paulobarreira.cursomc.services.exception.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+
+	@Autowired
+	private PedidoRepository repo;
+	
+	public Pedido buscar(Integer id) {
+		Optional<Pedido> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
+		}
+}
